8c5c80f0fe6d6bf40e5266166fb8fd6ac48abb38
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2024-10-12 10:18:58 +0200

    making a minst predictor using torch

---
+++ MNIST_TORCH/MINST.py
@@ -0,0 +1,25 @@
+import torch 
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+import matplotlib.pylab as plt
+import numpy as np
+import ssl
+import useful_functions
+
+#disable ssl to allow unverfied downloads due to 403 error from downloading the mnist datasets
+ssl._create_default_https_context = ssl._create_unverified_context
+
+#download the train and validation datasets and transfrom them to tensors 
+train_dataset = dsets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+validation_dataset = dsets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
+
+#verfying that data was correctly downloaded 
+print("Type of data element: ", type(train_dataset[0][1]))
+print("The label: ", train_dataset[3][1])
+print("\n")
+print("Image from train dataset")
+useful_functions.show_data(train_dataset[3])
+print("\n")
+print("Image from validation dataset")
+useful_functions.show_data(validation_dataset[1])
---
+++ MNIST_TORCH/useful_functions.py
@@ -0,0 +1,28 @@
+# The function to plot parameters
+import matplotlib.pylab as plt
+
+def PlotParameters(model): 
+    W = model.state_dict()['linear.weight'].data
+    w_min = W.min().item()
+    w_max = W.max().item()
+    fig, axes = plt.subplots(2, 5)
+    fig.subplots_adjust(hspace=0.01, wspace=0.1)
+    for i, ax in enumerate(axes.flat):
+        if i < 10:
+            
+            # Set the label for the sub-plot.
+            ax.set_xlabel("class: {0}".format(i))
+
+            # Plot the image.
+            ax.imshow(W[i, :].view(28, 28), vmin=w_min, vmax=w_max, cmap='seismic')
+
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+        # Ensure the plot is shown correctly with multiple plots
+        # in a single Notebook cell.
+    plt.show()
+
+def show_data(data_sample):
+    plt.imshow(data_sample[0].numpy().reshape(28, 28), cmap='gray')
+    plt.title('y = ' + str(data_sample[1]))
