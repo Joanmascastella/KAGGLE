6b324a9a84dd23c121ac5abb2f8e5737da193101
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2024-10-23 12:22:24 +0200

    creating structure

---
+++ Improvement/House Prediction/main.py
@@ -0,0 +1,9 @@
+
+
+
+# here have a main function that first initializes the data paths, these paths are then passed to data.py which
+# returns the cleaned data in dataloaders for both train and test
+# Then call the feature extraction.py to run an auto encoder which does feature extraction, this returns the
+# extracted features
+# after doing so pass those extracted features to the model compiler.py to run training and testing
+# and return the results and loss
